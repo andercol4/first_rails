bbef6948990f0e021c1c965dbb4b56ca6efb51a9
Colin Anderson <andercol.4@gmail.com>
2015-12-02 14:39:53 -0700

    Static Pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.all
   end
@@ -20,6 +20,20 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
   private
 
